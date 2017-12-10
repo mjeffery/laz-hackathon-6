c9bdc466dbe3d2a486196622f647590676521754
mjeffery <michael.jeffery@gmail.com>
2017-12-10 18:51:02 -0500

    update some of the pet accessors

--- test/pet-accessors.test.js
+++ test/pet-accessors.test.js
@@ -4,14 +4,25 @@ describe('pet-acccessors', () => {
 
     let examplePet = require('./resources/pet.json')
 
+    it('gets the animal', () => {
+        debugger;
+        let result = pet.getAnimal(examplePet)
+        expect(result).to.equal('cat')
+    })
+
     it('gets the pronoun', () => {
         let result = pet.getPronoun(examplePet)
         expect(result).to.equal('he')
     })
 
+    it("gets 'they' if no gender is defined", () => {
+        let result = pet.getPronoun(null)
+        expect(result).to.equal('they')
+    })
+
     it('gets the size', () => {
         let result = pet.getSize(examplePet)
-        expect(result).to.equal('medium')
+        expect(result).to.equal('a medium')
     })
 
     it('gets the age', () => {
